a02739cafafce5025cbd94a36aa6cab54359a699
RickyParte <rickydipakparte@gmail.com>
2024-04-27 13:50:05 +0530

    Unwanted Files

--- Employee/src/main/java/com/emp/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.emp.service;
-
-import org.springframework.stereotype.Service;
-
-
-public interface EmailService {
-    public void sendEmailNotificationToManager1(String toManagerEMail,String subject,String emailContent);
-
-}
